043057f4baef35370f27e531fce5413badd850a9
Ryan Moshi <ryan.moshi@strathmore.edu>
2025-07-13 22:48:55 +0300

    Update 170439_q1.py
    Updated small minor changes. On Parameters

--- 170439_q1.py
+++ 170439_q1.py
@@ -1,42 +1,74 @@
-# Library Management System
+# 170439_q1.py
+# This Is A Library Management System
 
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
         self.is_borrowed = False
-
-    def mark_borrowed(self):
-        self.is_borrowed = True
-
-    def mark_returned(self):
-        self.is_borrowed = False
-
-class LibraryMember:
-    def __init__(self, name, member_id):
-        self.name = name
-        self.member_id = member_id
-        self.borrowed_books = []
-
-    def borrow_book(self, book):
-        if not book.is_borrowed:
-            book.mark_borrowed()
-            self.borrowed_books.append(book)
-            print(f"{book.title} borrowed.")
+        
+    def borrow(self):
+        if self.is_borrowed == False:
+            self.is_borrowed = True
+            return f"The book has been checked out."
         else:
-            print("Book is already borrowed.")
-
+            return f"This book is already checked out."
+        
+    def return_book(self):
+        if self.is_borrowed == True:
+            self.is_borrowed = False
+            return f"The book has been returned."
+        else:
+            return f"This book wasn't checked out."
+        
+class LibraryMember:
+    def __init__(self, name, memberID):
+        self.name = name
+        self.memberID = memberID
+        self.borrowed_books = []
+        
+    def borrow_book(self, book):
+        if book.is_borrowed == False:
+            book.borrow()
+            self.borrowed_books.append(book)
+            return f"{book.title} has been borrowed by {self.name}."
+        else:
+            return f"{book.title} is not available right now."
+            
     def return_book(self, book):
         if book in self.borrowed_books:
-            book.mark_returned()
+            book.return_book()
             self.borrowed_books.remove(book)
-            print(f"{book.title} returned.")
+            return f"{book.title} has been returned by {self.name}."
         else:
-            print("You did not borrow this book.")
+            return f"{self.name} didn't borrow {book.title}."
+                
+    def ListBorrowedBooks(self):
+        if self.borrowed_books:
+            return [book.title for book in self.borrowed_books]
+        else:
+            return "No books currently borrowed."
 
-    def list_books(self):
-        if not self.borrowed_books:
-            print("No books borrowed.")
-        for b in self.borrowed_books:
-            print(b.title)
-
+if __name__ == "__main__":
+    book1 = Book("Python Fundamentals", "Kuol Abraham")
+    book2 = Book("Deep Learning with Python", "Joy Mbinya")
+    book3 = Book("AI for Beginners", "Samuel Githongori")
+    book4 = Book("Data Structures in Practice", "Eurel Owatte")
+    
+    member1 = LibraryMember("Ryan Moshi", 101)
+    member2 = LibraryMember("Brian Kikuvi", 102)
+    member3 = LibraryMember("Caleb Katumo", 103)
+    member4 = LibraryMember("Mumo Mwangagi", 104)
+    
+    print(member3.borrow_book(book2))
+    print(member4.borrow_book(book1))
+    print(member4.borrow_book(book1))
+    
+    print(member3.ListBorrowedBooks())
+    print(member4.ListBorrowedBooks())
+    
+    print("Books borrowed by Mumo:", member4.ListBorrowedBooks())
+    
+    print(member3.return_book(book2))
+    
+    print("Mumo's borrowed books after returning:", member4.ListBorrowedBooks())
